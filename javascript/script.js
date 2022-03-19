5606d9ffdd9e5b22fd3e6d00eee652e8eeeec7f9
AdeAde99 <adelintanasa.d@gmail.com>
2022-03-19 11:26:12 +0100

    prova js

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+const arrDifficoltà = [100,81,49];
+let i = 0;
+function playDiff() {
+    return i = document.getElementById('selezione').value;
+};
+
+document.addEventListener('click', playDiff);
+
+setTimeout()
+
+console = ()=> ({
+
+});
+console.log(arrDifficoltà[i]);
+console.log(i);
+
+
+
+
+
